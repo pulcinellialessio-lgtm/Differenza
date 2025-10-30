a5977ceded5156919221ce555594f7590273a773
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2025-10-30 09:13:30 +0100

    Aggiungere i file di progetto.

---
+++ Differenza/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Differenza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
